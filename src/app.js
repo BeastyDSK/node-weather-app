6ad834891acc27f640d988efbbe6515182f63880
BeastyDSK <dhawansaran14346@gmail.com>
2022-06-28 16:56:00 +0530

    HBS-HTML changed

--- src/app.js
+++ src/app.js
@@ -1,113 +1,94 @@
 // Loading the default and required npm modules
-const path = require('path')
-var sslRedirect = require('heroku-ssl-redirect');
-const express = require('express')
-const hbs = require('hbs')
-const chalk = require('chalk')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
-const app = express()
-
-
+const path = require("path");
+var sslRedirect = require("heroku-ssl-redirect");
+var compression = require("compression");
+var helmet = require("helmet");
+const express = require("express");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const app = express();
 
 // making the request to https
 
 app.use(sslRedirect());
+app.use(compression());
+app.use(helmet());
 
 // settig up the port to work with.
-const port = process.env.PORT || 1234
+const port = process.env.PORT || 1234;
 
 // setting up the default directories
-const PathforExpress = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../html/views')
-// const partialPath = path.join(__dirname,'../html/partials')
-
-//setting up the handle bar engine
-app.set('view engine','hbs')
-
-// setting up the path in which the handlebar loads the file
-// default is views deirectory
-app.set('views',viewsPath)
-
-app.use(express.static(PathforExpress))
-
-// registering handlebar partials
-// hbs.registerPartials(partialPath)
+const PathforExpress = path.join(__dirname, "../public");
+app.use(express.static(PathforExpress));
 
 // setting up the get request to send the data when some access the server
-app.get('',(req,res)=>{
-    res.render('index', {
-        title:'Weather'
-    })
-})
+app.get("/", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-
-app.get('/weather',(req,res)=>{
-    if(req.query.lat && req.query.lon){
-        geocode.withcoords(parseFloat(req.query.lat),parseFloat(req.query.lon),(Gerror,{place}={})=>{
-            if(Gerror){
+app.get("/weather", (req, res) => {
+    if (req.query.lat && req.query.lon) {
+        geocode.withcoords(parseFloat(req.query.lat), parseFloat(req.query.lon), (Gerror, { place } = {}) => {
+            if (Gerror) {
                 return res.send({
-                     error:Gerror
-                })
+                    error: Gerror,
+                });
             }
-            forecast(parseFloat(req.query.lon),parseFloat(req.query.lat),(Ferror,Fdata)=>{
-                if(Ferror){
+            forecast(parseFloat(req.query.lon), parseFloat(req.query.lat), (Ferror, Fdata) => {
+                if (Ferror) {
                     return res.send({
-                        error:Ferror
-                    })
+                        error: Ferror,
+                    });
                 }
                 res.send({
-                    temperature:parseFloat(Fdata.currently.temperature).toFixed(0),
-                    chance_for_rain:(Fdata.currently.precipProbability*100).toFixed(0),
-                    address:place,
-                    forecast:Fdata.currently.summary
-                })
-            })
-        })
-    }
-    else if(req.query.address){
-        geocode.withlocation(req.query.address,(Gerror,{lat,long,place}={})=>{
-            if(Gerror){
+                    temperature: parseFloat(Fdata.currently.temperature).toFixed(0),
+                    chance_for_rain: (Fdata.currently.precipProbability * 100).toFixed(0),
+                    address: place,
+                    forecast: Fdata.currently.summary,
+                });
+            });
+        });
+    } else if (req.query.address) {
+        geocode.withlocation(req.query.address, (Gerror, { lat, long, place } = {}) => {
+            if (Gerror) {
                 return res.send({
-                    error:Gerror
-                })
+                    error: Gerror,
+                });
             }
-            forecast(long,lat,(Ferror,Fdata)=>{
-                if(Ferror){
+            forecast(long, lat, (Ferror, Fdata) => {
+                if (Ferror) {
                     return res.send({
-                        error:Ferror
-                    })
+                        error: Ferror,
+                    });
                 }
                 res.send({
-                    temperature:  parseFloat(Fdata.currently.temperature).toFixed(0),
-                    chance_for_rain:(Fdata.currently.precipProbability*100).toFixed(0),
-                    address:place,
-                    forecast:Fdata.currently.summary
-                })
-            })
-        })
-    }
-    else{
+                    temperature: parseFloat(Fdata.currently.temperature).toFixed(0),
+                    chance_for_rain: (Fdata.currently.precipProbability * 100).toFixed(0),
+                    address: place,
+                    forecast: Fdata.currently.summary,
+                });
+            });
+        });
+    } else {
         res.send({
-            error:'Sorry you didn\'t provided any location.'
-        })
+            error: "Sorry you didn't provided any location.",
+        });
     }
-})
+});
 
+app.get("/about", (req, res) => {
+    res.send("I am working on it.");
+});
 
-app.get('/about',(req,res)=>{
-    res.send('I am working on it.')
-})
+app.get("/help", (req, res) => {
+    res.send("I am working on it.");
+});
 
-app.get('/help',(req,res)=>{
-    res.send('I am working on it.')
-})
+app.get("*", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "..", "public", "error.html"));
+});
 
-app.get('*',(req,res)=>{
-    res.render('error')
-})
-
-// setting up the server to start on localhost with a port of 1234 
-app.listen(port,()=>{
-    console.log(`server starting on ${port}.`)
-})
+// setting up the server to start on localhost with a port of 1234
+app.listen(port, () => {
+    console.log(`server starting on ${port}.`);
+});
